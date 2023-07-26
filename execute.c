c8e120ea7b852e7aa160107b3ad0822e7c61f683
Khaoulaad <ouajaa.khaoula@gmail.com>
2023-07-26 11:21:36 -0400

    0

--- execute.c
+++ execute.c
@@ -8,13 +8,13 @@
  */
 void execute_command(char *path_cmd, char *argv[])
 {
-        if (execve(path_cmd, argv, environ) == -1)
-        {
-                perror("Command not found");
-                if (path_cmd)
-                        free(path_cmd);
-                exit(EXIT_FAILURE);
-        }
+	if (execve(path_cmd, argv, environ) == -1)
+	{
+		perror("Command not found");
+		if (path_cmd)
+			free(path_cmd);
+		exit(EXIT_FAILURE);
+	}
 }
 
 /**
@@ -24,36 +24,36 @@ void execute_command(char *path_cmd, char *argv[])
  */
 int _execute(char *argv[])
 {
-        char *path_cmd = NULL;
-        pid_t born;
-        int status;
-        int built_in_flag = handl_builtin(argv[0], argv);
+	char *path_cmd = NULL;
+	pid_t born;
+	int status;
+	int built_in_flag = handl_builtin(argv[0], argv);
 
-        path_cmd = get_path(argv[0]);
+	path_cmd = get_path(argv[0]);
 
-        if (path_cmd == NULL && !built_in_flag)
-        {
-                perror(argv[0]);
-                return (100);
-        }
-if (built_in_flag)
-        {
-                if (path_cmd)
-                        free(path_cmd);
-                return (1);
-        }
-        born = fork();
-        if (born == -1)
-        {
-                perror("Error (fork)");
-                if (path_cmd)
-                        free(path_cmd);
-                exit(EXIT_FAILURE);
-        }
-        else if (born == 0)
-                execute_command(path_cmd, argv);
-        wait(&status);
-        if (path_cmd)
-                free(path_cmd);
-        return (status);
+	if (path_cmd == NULL && !built_in_flag)
+	{
+		perror(argv[0]);
+		return (100);
+	}
+	if (built_in_flag)
+	{
+		if (path_cmd)
+			free(path_cmd);
+		return (1);
+	}
+	born = fork();
+	if (born == -1)
+	{
+		perror("Error (fork)");
+		if (path_cmd)
+			free(path_cmd);
+		exit(EXIT_FAILURE);
+	}
+	else if (born == 0)
+		execute_command(path_cmd, argv);
+	wait(&status);
+	if (path_cmd)
+		free(path_cmd);
+	return (status);
 }
---
+++ string_functions2.c
@@ -0,0 +1,34 @@
+#include "shell.h"
+/**
+ * _puts - ...
+ * @str: ...
+ * Return: ...
+ */
+char _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	return (str[i]);
+}
+/**
+ * _strcpy - from dest to source
+ * @dest: destination
+ * @src: source
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
