449553a27bb928e63809eba88dec5a7f12c8dfa2
Khaoulaad <ouajaa.khaoula@gmail.com>
2023-07-26 16:03:33 -0400

    0

--- shell.c
+++ shell.c
@@ -1,49 +1,96 @@
 #include "shell.h"
+
 /**
- * main - entry point
- *
- * Return: 0 on success, -1 on failure
+ * handle_space_tab - check if there is a space or tab in the command
+ *@cmd: the command
+ *Return: 1 if there is a space or tab, 0 otherwise
  */
-
-int main(void)
+int handle_space_tab(char *cmd)
 {
-	char *lineptr = NULL, **cmd_argv = NULL;
-	size_t n = 0;
-	ssize_t number_of_char;
-	const char *delim = " \n";
-	int interactive_mode = isatty(STDIN_FILENO);
+	int i = 0;
 
-	while (1)
+	while (cmd[i])
 	{
-		if (interactive_mode)
-			write(1, "#shell$ ", 8);
-		number_of_char = getline(&lineptr, &n, stdin);
-		if (number_of_char == EOF)
-		{
-			putchar('\n');
-			break;
-		}
-		cmd_argv = tokenize_input(lineptr, delim);
-		if (cmd_argv == NULL)
-		{
-			free(lineptr);
-			exit(EXIT_FAILURE);
-		}
-		if (cmd_argv[0] == NULL || cmd_argv[0][0] == '\0')
-		{
-			free_cmd_argv(cmd_argv);
-			continue;
-		}
-		if (strcmp(cmd_argv[0], "env") == 0)
-			print_environment();
-		else if (strcmp(cmd_argv[0], "exit") == 0)
-		{
-			free_cmd_argv(cmd_argv);
-			break;
-		}
+		if (cmd[i] == ' ' || cmd[i] == '\t')
+			i++;
 		else
-			execute_cmd(cmd_argv);
-		free_cmd_argv(cmd_argv);
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * free_tokens - helper function
+ * @tokens: tokens to free
+ * Return: nothing
+ */
+void free_tokens(char **tokens)
+{
+	int i = 0;
+
+	while (tokens[i] != NULL)
+	{
+		free(tokens[i]);
+		i++;
+	}
+	free(tokens);
+}
+/**
+ * free_and_exit - frees allocated memory and exits the program
+ * @cmd: command string to free
+ */
+void free_and_exit(char *cmd)
+{
+	_putchar('\n');
+	free(cmd);
+	exit(EXIT_SUCCESS);
+}
+
+/**
+ * main - simple shell
+ * @argc: number of arguments
+ * @argv: arguments
+ * @envp: environment
+ * Return: 0 on success
+ */
+int main(__attribute__((unused)) int argc, char **argv __attribute__((unused)),
+		__attribute__((unused)) char **envp)
+{
+	char *lineptr = NULL, *prompt = "#shell$ ", *token;
+	char **token_copy;
+	ssize_t len = 0;
+	size_t size = 0;
+	bool flag = true;
+
+	while (1 && flag)
+	{
+		if (isatty(STDIN_FILENO) == 0)
+			flag = false;
+		else
+			_puts(prompt);
+		if (_getline(&lineptr, &size, stdin) == -1)
+			free_and_exit(lineptr);
+		len = _strlen(lineptr);
+		if (len > 0 && lineptr[len - 1] == '\n')
+			lineptr[len - 1] = '\0';
+		if (len == 1 || lineptr[0] == '\n' || handle_space_tab(lineptr) == 1)
+			continue;
+		shell_comments(lineptr);
+		if (lineptr[0] == '\0')
+			continue;
+		token = strtok(lineptr, ";\n");
+		while (token)
+		{
+			token_copy = _allocate_strtoke(token);
+			if (token_copy[0] == NULL || token_copy[0][0] == '\0')
+			{
+				token = strtok(NULL, ";\n");
+				continue;
+			}
+			_execute(token_copy);
+			free_tokens(token_copy);
+			token = strtok(NULL, ";\n");
+		}
 	}
 	free(lineptr);
 	return (0);
--- shell.h
+++ shell.h
@@ -3,20 +3,45 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <ctype.h>
+#include <stddef.h>
+#include <stdlib.h>
 
 extern char **environ;
 
-char *_getenv(const char *name);
-void print_environment(void);
-char *build_file_path(char *path_token, char *command);
-char *handle_path(char *command);
-void execute_cmd(char **argv);
-char **tokenize_input(char *lineptr, const char *delim);
-void free_cmd_argv(char **cmd_argv);
-void run_shell(void);
+/*functions_helper*/
 int _putchar(char c);
+int _strlen(char *s);
+char _puts(char *str);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
+char *_strdup(char *str);
+/*prototypes*/
+char *_getenv(char *PATH);
+int _execute(char *argv[]);
+char **_allocate_strtoke(char *value);
+int handl_builtin(char *cmd, char **args);
+void environment_fnc(char **args __attribute((unused)));
+void exit_fnc(char **args);
+char *get_path(char *command);
+int handle_space_tab(char *cmd);
+void free_and_exit(char *cmd);
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+void shell_comments(char *cmd);
+char *build_file_path(char *path_token, char *command);
+
+
+
 #endif
