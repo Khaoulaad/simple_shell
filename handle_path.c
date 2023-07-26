021ea969fcaa542c34790444cf76c36b40b0cc9c
zasamy <zainebzahiri@gmail.com>
2023-07-26 12:47:08 -0700

    handle_path file

--- handle_path.c
+++ handle_path.c
@@ -5,43 +5,44 @@
  * @command: commands
  * Return: path or null
  */
-
 char *get_path(char *command)
 {
-	char *path, *path_copy, *path_token, *file_path;
-	int command_length, directory_length;
-	struct stat buffer;
+	char *path, *hold_path_cpy, *file_path, *token;
+	int cmd_len, dir_len;
 
+	if (command[0] == '/' || command[0] == '.')
+	{
+		if (access(command, F_OK) == 0)
+			return (_strdup(command));
+		else
+			return (NULL);
+	}
 	path = getenv("PATH");
 	if (path)
 	{
-		path_copy = _strdup(path);
-		command_length = _strlen(command);
-		path_token = strtok(path_copy, ":");
-		while (path_token != NULL)
+		hold_path_cpy = _strdup(path);
+		cmd_len = _strlen(command);
+		token = strtok(hold_path_cpy, ":");
+		while (token)
 		{
-			directory_length = _strlen(path_token);
-			file_path = malloc(command_length + directory_length + 2);
-			_strcpy(file_path, path_token);
+			dir_len = _strlen(token);
+			file_path = malloc(sizeof(char) * (cmd_len + dir_len + 2));
+			_strcpy(file_path, token);
 			_strcat(file_path, "/");
 			_strcat(file_path, command);
 			_strcat(file_path, "\0");
-			if (stat(file_path, &buffer) == 0)
+			if (access(file_path, F_OK) == 0)
 			{
-				free(path_copy);
+				free(hold_path_cpy);
 				return (file_path);
 			}
 			else
 			{
 				free(file_path);
-				path_token = strtok(NULL, ":");
+				token = strtok(NULL, ":");
 			}
 		}
-		free(path_copy);
-		if (stat(command, &buffer) == 0)
-		{
-			return (command);
-		}
+		free(hold_path_cpy);
 		return (NULL);
 	}
 	return (NULL);
