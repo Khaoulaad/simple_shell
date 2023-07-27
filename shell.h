80971f2135ef593b006341c550a06d3c2cdb14b3
zasamy <zainebzahiri@gmail.com>
2023-07-27 04:06:24 -0700

    test

--- shell.h
+++ shell.h
@@ -18,49 +18,6 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-#define BUFFER_t 1024
-
-const char *errlist[] = {
-	"Undefined error",
-
-	"Operation not permitted",
-	"No such file or directory",
-	"No such process",
-	"Interrupted system call",
-	"I/O error",
-	"No such device or address",
-	"Argument list too long",
-	"Exec format error",
-	"Bad file number",
-	"No child processes",
-	"Try again",
-	"Out of memory",
-	"Permission denied",
-	"Bad address",
-	"Block device required",
-	"Device or resource busy",
-	"File exists",
-	"Cross-device link",
-	"No such device",
-	"Not a directory",
-	"Is a directory",
-	"Invalid argument",
-	"File table overflow",
-	"Too many open files",
-	"Not a typewriter",
-	"Text file busy",
-	"File too large",
-	"No space left on device",
-	"Illegal seek",
-	"Read-only file system",
-	"Too many links",
-	"Broken pipe",
-	"Too many processes",
-	"Too many users",
-	"Disc quota exceeded",
-	"No locks available",
-	"Inappropriate file type or format",
-};
 
 extern char **environ;
 extern char **__environ;
@@ -83,7 +40,7 @@ void exit_fnc(char **args);
 char *get_path(char *command);
 int handle_space_tab(char *cmd);
 void free_and_exit(char *cmd);
-int _getline(char **lnptr, size_t *size, int fd);
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
 void shell_comments(char *cmd);
 char *build_file_path(char *path_token, char *command);
 char *_getenv(char *PATH);
