f7ef9ca5a1e20525ae41f29cf802be608a7fe478
zasamy <zainebzahiri@gmail.com>
2023-07-28 17:55:02 -0700

    modif

--- _getline.c
+++ _getline.c
@@ -9,40 +9,40 @@
  * Return: the lenghh of lnptr or -1 on failure
  */
 
-int _getline(char **lnptr, size_t *size, int fd)
+int _getline(char **lineptr, size_t *size, int fd)
 {
 	char buffer[BUFFER_t + 1];
 	int r = BUFFER_t, len = 0, mode = isatty(0);
 	char *tmp;
 
-	*lnptr = (char *)_malloc(4);
-	**lnptr = '\0';
+	*lineptr = (char *)_malloc(4);
+	**lineptr = '\0';
 	while (r == BUFFER_t)
 	{
-		if (*lnptr && len > 0)
-			if ((*lnptr)[len - 1] == '\n' && mode)
+		if (*lineptr && len > 0)
+			if ((*lineptr)[len - 1] == '\n' && mode)
 				break;
 		r = read(fd, buffer, BUFFER_t);
 		if (r < 0 && errno == EINTR)
 		{
-			**lnptr = '\n',	*(*lnptr + 1) = '\n', len = 2;
+			**lineptr = '\n',	*(*lineptr + 1) = '\n', len = 2;
 			return (len);
 		}
 		if (r < 0)
 			exit(-1);
 		if (r == 0 && mode)
 			return (-1);
-		tmp = _realloc(*lnptr, len + r + 4);
-		*size = len + r + 4, *lnptr = tmp;
-		buffer[r] = '\0', _strcat(*lnptr, buffer), len += r;
+		tmp = _realloc(*lineptr, len + r + 4);
+		*size = len + r + 4, *lineptr = tmp;
+		buffer[r] = '\0', _strcat(*lineptr, buffer), len += r;
 		if (!mode)
-			find_n_rep(lnptr, "\n", ";");
+			find_n_rep(lineptr, "\n", ";");
 	}
 	if (!mode)
 	{
-		tmp = _realloc(*lnptr, _strlen(*lnptr) + 3);
-		*lnptr = tmp;
-		(*lnptr)[len] = '\n', (*lnptr)[len + 1] = '\0';
+		tmp = _realloc(*lineptr, _strlen(*lineptr) + 3);
+		*lineptr = tmp;
+		(*lineptr)[len] = '\n', (*lineptr)[len + 1] = '\0';
 	}
 	return (len);
 }
